c147ee9414e4e0a96616af90e87136560131ff6a
NagaKanaparthy <NagaKanaparthy@users.noreply.github.com>
2017-02-07 17:09:18 -0500

    added auto yes

--- ROS/x86-Kinetic.sh
+++ ROS/x86-Kinetic.sh
@@ -5,12 +5,12 @@ echo "updating locals and repositories"
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
+sudo apt-get update -y
 
 echo "Installing ROS"
-sudo apt-get install ros-kinetic-desktop-full
-sudo apt-get install python-rosdep
-sudo apt-get install python-rosinstall
+sudo apt-get install ros-kinetic-desktop-full -y
+sudo apt-get install python-rosdep -y
+sudo apt-get install python-rosinstall y-
 sudo rosdep init
 rosdep update
 
